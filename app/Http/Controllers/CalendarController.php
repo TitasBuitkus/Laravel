88c684581c316c377cf163b2ece63df70baca3ca
TitasBuitkus <titas.buitkus@stud.vilniustech.lt>
2024-12-02 14:33:02 +0200

    Darbinis #2

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -11,37 +11,51 @@ class CalendarController extends Controller
     {
         $events = array();
         $bookings = Booking::all();
-        foreach ($bookings as $booking) {
+        foreach($bookings as $booking) {
+            $color = null;
+            if($booking->title == 'Test') {
+                $color = '#924ACE';
+            }
+            if($booking->title == 'Test 1') {
+                $color = '#68B01A';
+            }
+
             $events[] = [
+                'id'   => $booking->id,
                 'title' => $booking->title,
                 'start' => $booking->start_date,
                 'end' => $booking->end_date,
-        ];
-
+                'color' => $color
+            ];
         }
-
         return view('calendar.index', ['events' => $events]);
     }
     public function store(Request $request)
     {
-
         $request->validate([
             'title' => 'required|string'
         ]);
 
         $booking = Booking::create([
-            'title' => $request->get('title'),
-            'start_date' => $request->get('start_date'),
-            'end_date' => $request->geti('end_date'),
+            'title' => $request->title,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
         ]);
-        $booking -> save();
+
+        $color = null;
+
+        if($booking->title == 'Test') {
+            $color = '#924ACE';
+        }
+
         return response()->json([
             'id' => $booking->id,
             'start' => $booking->start_date,
             'end' => $booking->end_date,
             'title' => $booking->title,
-        ]);
+            'color' => $color ? $color: '',
 
+        ]);
     }
     public function update(Request $request ,$id)
     {
@@ -68,5 +82,4 @@ class CalendarController extends Controller
         $booking->delete();
         return $id;
     }
-
 }
