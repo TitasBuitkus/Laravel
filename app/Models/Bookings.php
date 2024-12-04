101108dc6c4ee1530fa7d15073311532d26f81d9
TitasBuitkus <titas.buitkus@stud.vilniustech.lt>
2024-12-04 14:05:10 +0200

    Darbinis #4

--- app/Models/Bookings.php
+++ app/Models/Bookings.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bookings extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'start_date', 'end_date'];
+
 }
